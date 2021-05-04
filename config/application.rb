6c5afdbd341b290110aff1e0c1ff7e9416296b25
Newburu <newburu@gmail.com>
2021-05-04 14:58:23 +0900

    slim導入

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module SalonReview
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.template_engine = :slim #slimに変更
   end
 end
