700fd07bb7a7a466c47631910cdcc092c3e8b68b
Newburu <newburu@gmail.com>
2021-05-05 13:47:01 +0900

    dependent: :destroyを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :salon
   has_many :review_points
-  has_many :points, through: :review_points
+  has_many :points, through: :review_points, dependent: :destroy
   accepts_nested_attributes_for :review_points
 end
